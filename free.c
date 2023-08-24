bf4128df13f046ca505cb994b64ed67b3a0b142b
Constance Amatey <consamate57@gmail.com>
2023-08-24 04:55:35 -0700

    op code match

---
+++ free.c
@@ -0,0 +1,20 @@
+/**
+ * free_stack - frees all nodes in a stack
+ * @stack: pointer to the head node pointer of stack
+ *
+ * Return: Nothing.
+ */
+void free_stack(stack_t **head)
+{
+	stack_t *tmp = NULL;
+
+	if (!head || !(*head) == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
--- monty.h
+++ monty.h
@@ -7,7 +7,7 @@
 
 #define TRUE 1
 
-**
+/**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
@@ -24,6 +24,19 @@ typedef struct stack_s
 } stack_t;
 
 /**
+ * struct line - contents of line and corresponding number
+ * @contents: array of tokens read from the line
+ * @number: the line number
+ *
+ * Description: contents of a line and corresponding number
+ */
+typedef struct lines
+{
+	unsigned int number;
+	char **content;
+} line_t;
+
+/**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
@@ -37,6 +50,35 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/**
+ * struct stack_s - struct for memory allocation
+ * @buf: buffer
+ * @stack: stack node
+ * @file: file
+ */
+typedef struct mem_s
+{
+	char *buf;
+	stack_t *stack;
+	FILE *file;
+} mem_t;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack
+ * @next: points to the next element of the stack
+ *
+ * Description: Doubly linked list struct for stacks and queus project
+ */
+typedef struct arg_s
+{
+	int arg;
+	int flag;
+} arg_t;
+
+extern arg_t arg;
+
 void _push(stack_t **head, (__attribute__((unused))unsigned int line));
 void _pall(stack_t **head, (__attribute__((unused))unsigned int line));
 void _pint(stack_t **head, unsigned int line);
@@ -44,5 +86,6 @@ void _pop(stack_t **head, unsigned int line);
 void _swap(stack_t **head, unsigned int line);
 void _add(stack_t **head, unsigned int line);
 void _nop(stack_t **head, __attribute__((unused)) unsigned int line);
+void (*op_match(line_t lines, mem_t *mem)(stack_t **, unsigned int));
 
 #endif

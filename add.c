8ccd05a4681f8a6ae6fe249a52321a472f7a647e
Constance Amatey <consamate57@gmail.com>
2023-08-24 00:30:04 -0700

    added push and pall functions

---
+++ add.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+
+/**
+ * _push - pushes a new node to the top of the stack
+ * @head: pointer to the head
+ * @line: line counter
+ * Return: void
+ */
+void _push(stack_t **head, (__attribute__((unused))unsigned int line))
+{
+	stack_t newNode;
+
+	newNode = malloc(sizeof(stack_t));
+
+	if (newNode == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_stack(head);
+		exit(EXIT FAILURE);
+	}
+
+	newNode->next = *head;
+	newNode->prev = NULL;
+	newNode->n = arg.arg;
+	if (*head)
+	{
+		(*head)->prev = newNode;
+	}	
+	*head = newNode;
+}
+
+
+
+
+
+/**
+ * _pall - prints all node data of the stack
+ * @head: pointer to the head
+ * @line: line counter
+ * Return: void
+ */
+void _pall(stack_t **head, (__attribute__((unused))unsigned int line))
+{
+	stack_t *tmp;
+
+	tmp = *head;
+	while (tmp)
+	{
+		printf("%d/n", tmp->n);
+		tmp = tmp->next;
+	}
+}
